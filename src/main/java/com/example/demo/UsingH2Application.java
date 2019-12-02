f79980656d37fe132608b4d3931b4045cadbf64a
eDelaMora <eDelaMora@CLQL001412>
2019-12-02 11:44:26 -0800

    create spring mvc project

---
+++ src/main/java/com/example/demo/UsingH2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingH2Application {
+
+	public static void main(String[] args) {
+		 /* Responsible for launching the boot application. */
+		SpringApplication.run(UsingH2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/constant/ConstantVariable.java
@@ -0,0 +1,5 @@
+package com.example.demo.constant;
+
+public class ConstantVariable {
+	 public static String EMPLOYEE_LIST = "employees";
+}
